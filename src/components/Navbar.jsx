7fae370cecae3fce6ebee9a089462eff4acc0774
renzynx <renzynx@gmail.com>
2022-03-21 15:54:06 +0700

    Initial commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import style from "../styles/Navbar.module.css";
+
+const Navbar = () => {
+	return (
+		<div>
+			<button className={style.btn}>Hello World</button>
+		</div>
+	);
+};
+
+export default Navbar;
---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import Head from "next/head";
+import Navbar from "../components/Navbar";
+
+export default function Home() {
+	return (
+		<div>
+			<Head>
+				<title>CheetaPanel | Home</title>
+			</Head>
+			<Navbar />
+		</div>
+	);
+}
