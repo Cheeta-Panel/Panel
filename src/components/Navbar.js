8afc0ac627c90b01ee96bbde682ac8cbd83eee03
zinedinarnaut <zinedinarnaut085@gmail.com>
2022-03-22 23:02:50 +1100

    Panel files

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -13,6 +13,7 @@ function Navbar() {
 		<>
 			<Nav>
 				<NavName href="/">
+
 				</NavName>
 				<div className="menu-icon" onClick={handleClick}>
 					<i className={click ? "fas fa-times" : "fas fa-bars"}/>
